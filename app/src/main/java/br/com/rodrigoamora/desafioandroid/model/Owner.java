503938339d340e1ea59e47bfb7d51048fa444acf
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-08-07 22:17:53 -0300

    Craindo a classe Owner

---
+++ app/src/main/java/br/com/rodrigoamora/desafioandroid/model/Owner.java
@@ -0,0 +1,58 @@
+package br.com.rodrigoamora.desafioandroid.model;
+
+public class Owner {
+
+    private Long id;
+    private String login, avatar_url;
+    private String html_url, repos_url;
+    private Owner owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+
+    public void setHtml_url(String html_url) {
+        this.html_url = html_url;
+    }
+
+    public String getRepos_url() {
+        return repos_url;
+    }
+
+    public void setRepos_url(String repos_url) {
+        this.repos_url = repos_url;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+}
